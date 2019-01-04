de1d61eabac52596b7a5c7e1ae33b780a0b66678
Giannis Karastatiras <gianniskarastatiras@gmail.com>
2019-01-04 21:00:34 +0200

    Add files via upload

---
+++ ViewModels/InstructorsData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BootCampApp.Models;
+
+namespace BootCampApp.ViewModels
+{
+    public class InstructorsData    
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+
+    }
+}
